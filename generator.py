5882116686661650ea568b024c978bcbfad3b706
Th3VaultDweller <aztec242@yandex.ru>
2023-10-21 17:54:15 +0300

    Testing

--- generator.py
+++ generator.py
@@ -130,7 +130,7 @@ print(f"Here's your password: {generated_password}")
 
 input("Run the app again to generate a new one? y/N")
 if input == "y":
-    print(f"Here's another password: {generated_password}")
+    # print(f"Here's another password: {generated_password}")
     f = open("generated_passwords.txt", "a")
     f.write(f"{today:%B %d, %Y}\n Here's your password: {(str(password(1))[:25])} \r\n")
     f.close
