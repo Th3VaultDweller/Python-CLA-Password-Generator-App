1df19e33222e48aaa10eddb82f0c5256fd7a1085
Th3VaultDweller <aztec242@yandex.ru>
2023-10-22 15:11:17 +0300

    Minor tweaks and fixes

--- generator.py
+++ generator.py
@@ -7,36 +7,38 @@ path = "generated_passwords.txt"
 
 
 def password(random_symbols):
-    random_symbols = random.choices(string.hexdigits, k=5)
-    +random.choices(string.hexdigits, k=5)
+    random_symbols = list(
+        random.choices(string.hexdigits, k=5)
+        + random.choices(string.punctuation, k=3)
+        + random.choices(string.hexdigits, k=5)
+    )
     return random_symbols
 
 
 generated_password = str(password(1))[:200]
 today = datetime.datetime.today()  # дата создания пароля
+
+
 if path:
     # если файл существует, в него прикрепляется сгенерированный пароль
     f = open("generated_passwords.txt", "a")
-    f.write(
-        f"{today:%B %d, %Y}\n Here's your another password: 
-        \r\n {generated_password} \r\n")
+    f.write(f"{today:%B %d, %Y}\n Here's your password: \r\n {generated_password} \r\n")
     f.close
 else:
     # если файла не существует, он создаётся, и в него записывается пароль
     f = open("generated_passwords.txt", "w+")
-    f.write(
-        f"{today:%B %d, %Y}\n Here's your password: 
-        \r\n {generated_password} \r\n")
+    f.write(f"{today:%B %d, %Y}\n Here's your password: \r\n {generated_password} \r\n")
     f.close
 
 
 print(f"Here's your password: {(generated_password)}")
 
-input("Run the app again to generate a new one? y/N")
-if input == "y":
-    # print(f"Here's another password: {generated_password}")
-    f = open("generated_passwords.txt", "a")
-    f.write(
-        f"{today:%B %d, %Y}\n Here's your password: \r\n {(str(password(1))[:200])} \r\n"
-    )
-    f.close
+
+# input("Run the app again to generate a new one? y/N")
+# if input == "y":
+#     # print(f"Here's another password: {generated_password}")
+#     f = open("generated_passwords.txt", "a")
+#     f.write(
+#         f"{today:%B %d, %Y}\n Here's your password: \r\n {(str(password(1))[:200])} \r\n"
+#     )
+#     f.close
