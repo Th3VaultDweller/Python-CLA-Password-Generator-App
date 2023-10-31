ef4b57661715a779cfd1c15a0db84bdacdf4decb
Th3VaultDweller <aztec242@yandex.ru>
2023-10-31 23:35:56 +0300

    Testing While loop

--- generator.py
+++ generator.py
@@ -15,31 +15,38 @@ def get_password(random_symbols):
     return random_symbols
 
 
+def user_input(self):
+    input("Run the app again to generate a new one? Y/N\n")
+
+
+def write_password(self):
+    if path:
+        # если файл существует, в него прикрепляется сгенерированный пароль
+        f = open("generated_passwords.txt", "a")
+        f.write(
+            f"{today:%B %d, %Y}\n Here's your password: {password_no_brackets} \r\n"
+        )
+        f.close
+    else:
+        # если файла не существует, он создаётся, и в него записывается пароль
+        print(f"Here's your password: {(password_no_brackets)}")
+        f = open("generated_passwords.txt", "w+")
+        f.write(
+            f"{today:%B %d, %Y}\n Here's your password: {password_no_brackets} \r\n"
+        )
+        f.close
+
+
 generated_password = (get_password(1))[:200]  # есть скобки, пробелы и запятые
 password_no_brackets = "".join(generated_password)
 today = datetime.datetime.today()  # дата создания пароля
 
-print(f"Here's your password: {(password_no_brackets)}")
-
 while True:
-    repeat = input("Run the app again to generate a new one? Y/N\n")
-    if repeat == "Y" or "y":
-        print(f"Here's your password: {(password_no_brackets)}")
-        if path:
-            # если файл существует, в него прикрепляется сгенерированный пароль
-            f = open("generated_passwords.txt", "a")
-            f.write(
-                f"{today:%B %d, %Y}\n Here's your password: {password_no_brackets} \r\n"
-            )
-            f.close
-        else:
-            # если файла не существует, он создаётся, и в него записывается пароль
-            print(f"Here's your password: {(password_no_brackets)}")
-            f = open("generated_passwords.txt", "w+")
-            f.write(
-                f"{today:%B %d, %Y}\n Here's your password: {password_no_brackets} \r\n"
-            )
-            f.close
-    else:
+    print(f"Here's your password: {(password_no_brackets)}")
+    user_input(1)
+    if path:
+        write_password(1)
+
+    if user_input == "N" or "n":
         print("Closing the app. See you soon")
         break
