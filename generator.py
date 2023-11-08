0c62fa2b19380684f60e02abc691b35e9ccec703
Th3VaultDweller <aztec242@yandex.ru>
2023-11-08 21:10:40 +0300

    Refactored the code

--- generator.py
+++ generator.py
@@ -6,20 +6,20 @@ from os.path import exists, isfile
 
 path = "generated_passwords.txt"
 
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
 
-def get_password(random_symbols):
-    random_symbols = list(
-        secrets.choice(string.hexdigits)
-        + secrets.choice(string.punctuation)
-        + secrets.choice(string.hexdigits)
-        + secrets.choice(string.hexdigits)
-        + secrets.choice(string.punctuation)
-        + secrets.choice(string.hexdigits)
-        + secrets.choice(string.hexdigits)
-        + secrets.choice(string.punctuation)
-        + secrets.choice(string.hexdigits)
-    )
-    return random_symbols
+alphabet = letters + digits + special_chars
+
+password_length = 15
+
+
+def get_password(password_length):
+    my_password = ""
+    for i in range(password_length):
+        my_password += "".join(secrets.choice(alphabet))
+    return my_password
 
 
 def user_input(self):
@@ -31,32 +31,28 @@ def write_password_to_file(self):
         # если файл существует, в него прикрепляется сгенерированный пароль
         f = open("generated_passwords.txt", "a")
         f.write(
-            f"{today:%B %d, %Y}\n Here's your password: {password_no_brackets} \r\n"
+            f"{today:%B %d, %Y}\n Here's your password: {get_password(password_length)} \r\n"
         )
         f.close
     else:
         # если файла не существует, он создаётся, и в него записывается пароль
-        print(f"Here's your password: {(password_no_brackets)}")
+        print(f"Here's your password: {(get_password(password_length))}")
         f = open("generated_passwords.txt", "w+")
         f.write(
-            f"{today:%B %d, %Y}\n Here's your password: {password_no_brackets} \r\n"
+            f"{today:%B %d, %Y}\n Here's your password: {get_password(password_length)} \r\n"
         )
         f.close
 
 
-generated_password = (get_password(1))[:200]  # есть скобки, пробелы и запятые
-password_no_brackets = "".join(generated_password)
 today = datetime.datetime.today()  # дата создания пароля
 
 while True:
-    if user_input(1) == "Y" or "y":
-        print(f"Here's your password: {(password_no_brackets)}")
-        # вывод и запись одного и того же пароля в терминал и в файл
-        write_password_to_file(1)
-        print(f"Here's your another password: {(password_no_brackets)}")
+    print(f"Here's your password: {(get_password(password_length))}")
+    user_input(1) == "Y" or "y"
+    # вывод и запись одного и того же пароля в терминал и в файл
+    write_password_to_file(1)
+    print(f"Here's your another password: {(get_password(password_length))}")
 
     if user_input == "N" or "n":
         print("Closing the app. See you soon.")
         break
-
-    # user_input(1)
